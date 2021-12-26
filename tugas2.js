b88d1025ddfe95f39e0b04989da6f079693a2a16
Albert <akuanakrajin7@gmail.com>
2021-12-26 22:06:31 +0700

    Tugas 2 JavaScript

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number = 10;
+var string = "Belajar Di Niomic";
+var boolean = true;
+var kosong = null;
+var array = [20,10,21,34];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(kosong);
+console.log(array);
